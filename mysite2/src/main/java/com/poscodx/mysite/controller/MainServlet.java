88d4e8700b554462e200abe6340e2e4d64bed415
CHOI HyoJun <cjfgh2449@gmail.com>
2024-05-29 10:27:01 +0900

    feat. servlet context's component

--- mysite2/src/main/java/com/poscodx/mysite/controller/MainServlet.java
+++ mysite2/src/main/java/com/poscodx/mysite/controller/MainServlet.java
@@ -1,8 +1,20 @@
 package com.poscodx.mysite.controller;
 
+import javax.servlet.ServletException;
+
 import com.poscodx.mysite.controller.action.main.MainAction;
 
 public class MainServlet extends ActionServlet {
+	@Override
+	public void init() throws ServletException {
+		String config = getServletConfig().getInitParameter("config");
+		System.out.println("MainController.init() called: " + config);
+		
+		
+		super.init();
+		
+	}
+
 	private static final long serialVersionUID = 1L;
 
 	@Override
