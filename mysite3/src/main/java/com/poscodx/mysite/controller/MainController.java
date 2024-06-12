c83158c3cbc2ed0b73ac02e55714be541b6cdf7f
CHOI HyoJun <cjfgh2449@gmail.com>
2024-06-12 15:37:51 +0900

    feat. user api

--- mysite3/src/main/java/com/poscodx/mysite/controller/MainController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/MainController.java
@@ -3,9 +3,11 @@ package com.poscodx.mysite.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.poscodx.mysite.service.SiteService;
 import com.poscodx.mysite.vo.SiteVo;
+import com.poscodx.mysite.vo.UserVo;
 
 @Controller
 public class MainController {
@@ -23,5 +25,27 @@ public class MainController {
 		model.addAttribute("sitevo", vo);
 		return "main/index";
 	}
+	
+	@ResponseBody
+	@RequestMapping("/msg01")
+	public String message01() {
+		return "Hello World";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/msg02")
+	public String message02(String name) {
+		return "안녕~ " + name;
+	}
 
+	@ResponseBody
+	@RequestMapping("/msg03")
+	public Object message03() {
+		UserVo vo = new UserVo();
+		vo.setNo(1L);
+		vo.setName("둘리");
+		vo.setEmail("dooly@gmail.com");
+		
+		return vo; 
+	}
 }
