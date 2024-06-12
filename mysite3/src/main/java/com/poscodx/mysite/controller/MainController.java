91148c2f74713ebeac45903e6418382dc69ae8a1
CHOI HyoJun <cjfgh2449@gmail.com>
2024-06-12 11:31:41 +0900

    feat. mysite3 admin 변경 과제 #finish

--- mysite3/src/main/java/com/poscodx/mysite/controller/MainController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/MainController.java
@@ -1,9 +1,11 @@
 package com.poscodx.mysite.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.poscodx.mysite.service.SiteService;
+import com.poscodx.mysite.vo.SiteVo;
 
 @Controller
 public class MainController {
@@ -12,11 +14,13 @@ public class MainController {
 	
 	public MainController(SiteService siteService) {
 		this.siteService = siteService;
+		
 	}
 	
 	@RequestMapping({"/", "/main"})
-	public String index() {
-
+	public String index(Model model) {
+		SiteVo vo = siteService.getSite();
+		model.addAttribute("sitevo", vo);
 		return "main/index";
 	}
 
