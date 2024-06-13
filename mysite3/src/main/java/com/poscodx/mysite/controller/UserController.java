2b4067ae6498e553be27c066269ceb25f0aea773
CHOI HyoJun <cjfgh2449@gmail.com>
2024-06-13 17:31:44 +0900

    feat. validation #finish

--- mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.poscodx.mysite.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,9 +28,32 @@ public class UserController {
 	public String join() {
 		return "user/join";
 	}
+//	
+//	@RequestMapping(value="/join", method=RequestMethod.POST)
+//	public String join(UserVo vo) {
+//		userService.join(vo);
+//		return "redirect:/user/joinsuccess";
+//	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(UserVo vo) {
+	public String join(@ModelAttribute @Valid UserVo vo, BindingResult result, Model model){
+		if(result.hasErrors()) {
+//			model.addAttribute("userVo", vo);
+
+//			List<ObjectError> list = result.getAllErrors();
+//			for(ObjectError error:list) {
+//				System.out.println(error);
+//			}
+			Map<String, Object> map = result.getModel();			
+//			Set<String> s = map.keySet();
+//			for(String key : s) {
+//				model.addAttribute(key, map.get(key));
+//			}
+			model.addAllAttributes(map);
+			
+			return "user/join";
+		}
+		
 		userService.join(vo);
 		return "redirect:/user/joinsuccess";
 	}
