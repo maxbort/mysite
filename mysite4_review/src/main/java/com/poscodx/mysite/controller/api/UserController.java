f9e30e61963f0773197bb1674776e8c017fc3f45
CHOI HyoJun <cjfgh2449@gmail.com>
2024-06-27 16:52:37 +0900

    Java Configuration (xml -> java로 설정 변경) 복습 #1

---
+++ mysite4_review/src/main/java/com/poscodx/mysite/controller/api/UserController.java
@@ -0,0 +1,38 @@
+package com.poscodx.mysite.controller.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.poscodx.mysite.dto.JsonResult;
+import com.poscodx.mysite.service.UserService;
+import com.poscodx.mysite.vo.UserVo;
+
+@RestController("userApiController")
+@RequestMapping("/user/api")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/checkemail")
+	public JsonResult checkEmail(@RequestParam(value="email", required=true, defaultValue="") String email) {
+		UserVo vo = userService.getUser(email);
+//		JsonResult jsonResult=  new JsonResult();
+//		
+//		jsonResult.setResult("ok");
+//		
+//		return jsonResult;
+		
+		return JsonResult.success(vo!=null);
+	}
+	
+	
+	
+//	public Object checkEmail(@RequestParam(value="email", required=true, defaultValue="") String email) {
+//			UserVo vo = userService.getUser(email);	
+//			return Map.of("exists", vo != null);
+//		}
+}
