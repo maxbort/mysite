02c9ce89c471e1393aa09c8c961e4f5346bf5ee2
CHOI HyoJun <cjfgh2449@gmail.com>
2024-06-28 15:23:22 +0900

    feat. mysite5_review #start

---
+++ mysite5_review/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -0,0 +1,153 @@
+package com.poscodx.mysite.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.poscodx.mysite.dto.JsonResult;
+import com.poscodx.mysite.security.Auth;
+import com.poscodx.mysite.security.AuthUser;
+import com.poscodx.mysite.service.UserService;
+import com.poscodx.mysite.vo.UserVo;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/join", method=RequestMethod.GET)
+	public String join(@ModelAttribute UserVo vo) {
+		return "user/join";
+	}
+	
+	@GetMapping("/checkemail")
+	public JsonResult checkEmail(@RequestParam(value="email", required=true, defaultValue = "") String email) {
+		UserVo vo = userService.getUser(email);
+	
+		return JsonResult.success(vo != null);
+		
+//		JsonResult jsonResult = new JsonResult();
+//		
+//		jsonResult.setResult("ok");
+//		
+//		return jsonResult;
+//				
+	}
+//	
+//	@RequestMapping(value="/join", method=RequestMethod.POST)
+//	public String join(UserVo vo) {
+//		userService.join(vo);
+//		return "redirect:/user/joinsuccess";
+//	}
+	
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(@ModelAttribute @Valid UserVo vo, BindingResult result, Model model){
+		if(result.hasErrors()) {
+//			model.addAttribute("userVo", vo);
+
+//			List<ObjectError> list = result.getAllErrors();
+//			for(ObjectError error:list) {
+//				System.out.println(error);
+//			}
+			Map<String, Object> map = result.getModel();
+//			Set<String> s = map.keySet();
+//			for(String key : s) {
+//				model.addAttribute(key, map.get(key));
+//			}
+			model.addAllAttributes(map);
+			
+			return "user/join";
+		}
+		
+		userService.join(vo);
+		return "redirect:/user/joinsuccess";
+	}
+
+	@RequestMapping(value="/joinsuccess", method=RequestMethod.GET)
+	public String joinsuccess() {
+		return "user/joinsuccess";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String login() {
+		return "user/login";
+	}
+	
+//	@RequestMapping(value="/login", method=RequestMethod.POST)
+//	public String login(HttpSession session, UserVo vo, Model model) {
+//		UserVo authUser = userService.getUser(vo.getEmail(), vo.getPassword());
+//		if(authUser == null) {
+//			model.addAttribute("email", vo.getEmail());
+//			model.addAttribute("result", "fail");
+//			
+//			return "user/login";
+//		}
+//		
+//		// login 처리
+//		session.setAttribute("authUser", authUser);
+//		
+//		return "redirect:/";
+//	}
+	
+//	@RequestMapping("/logout")
+//	public String logout(HttpSession session) {
+//		session.removeAttribute("authUser");
+//		session.invalidate();
+//		return "redirect:/";
+//	}
+	
+	@Auth
+	@RequestMapping(value="/update", method=RequestMethod.GET)
+	public String update(@AuthUser UserVo authUser, Model model) {
+		// access control
+//		UserVo authUser = (UserVo)session.getAttribute("authUser");
+//		if(authUser == null) {
+//			return "redirect:/";
+//		}
+//		////////////////////////
+		
+		UserVo vo = userService.getUser(authUser.getNo());
+		model.addAttribute("userVo", vo);
+		
+		return "user/update";
+	}
+	
+	@Auth
+	@RequestMapping(value="/update", method=RequestMethod.POST)
+	public String update(HttpSession session, UserVo vo) {
+		// access control
+		UserVo authUser = (UserVo)session.getAttribute("authUser");
+//		if(authUser == null) {
+//			return "redirect:/";
+//		}
+//		////////////////////////
+
+		vo.setNo(authUser.getNo());
+		userService.update(vo);
+		
+		authUser.setName(vo.getName());
+		return "redirect:/";
+	}
+	
+
+	@RequestMapping("/auth")
+	public void auth() {
+	}
+
+	@RequestMapping("/logout")
+	public void logout() {
+	}
+	
+}
---
+++ mysite5_review/src/main/webapp/WEB-INF/views/board/view.jsp
@@ -0,0 +1,51 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>mysite3</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<link href="${pageContext.request.contextPath}/assets/css/board.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div id="container">
+	<c:import url="/WEB-INF/views/includes/header.jsp" />
+
+		<div id="content">
+			<div id="board" class="board-form">
+				<table class="tbl-ex">
+					<tr>
+						<th colspan="2">글보기</th>
+					</tr>
+					<tr>
+						<td class="label">제목</td>
+						<td>${vo.title }</td>
+					</tr>
+					<tr>
+						<td class="label">내용</td>
+						<td>
+							<div class="view-content">
+								${vo.contents }
+							</div>
+						</td>
+					</tr>
+				</table>
+				<div class="bottom">
+					<a href="${pageContext.request.contextPath }/board?page=${param.page }&kwd=${param.kwd }">글목록</a>
+					<c:if test="${ not empty authUser }">
+						<a href="${pageContext.request.contextPath }/board/reply/${vo.no }?page=${param.page }&kwd=${param.kwd }">답글 달기</a>
+						<c:if test="${authUser.no == vo.userNo }">
+							<a href="${pageContext.request.contextPath }/board/modify/${vo.no }?page=${param.page }&kwd=${param.kwd }">글수정</a>
+						</c:if>
+					</c:if>
+				</div>
+			</div>
+		</div>
+		<c:import url="/WEB-INF/views/includes/navigation.jsp" />
+		<c:import url="/WEB-INF/views/includes/footer.jsp" />
+	</div>
+</body>
+</html>
