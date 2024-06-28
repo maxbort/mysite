462b701790c967a5e26a262a373d632770e76e92
CHOI HyoJun <cjfgh2449@gmail.com>
2024-06-28 18:05:15 +0900

    feat. Spring Security 적용 #02

--- mysite5_review/src/main/java/com/poscodx/mysite/config/app/SecurityConfig.java
+++ mysite5_review/src/main/java/com/poscodx/mysite/config/app/SecurityConfig.java
@@ -2,13 +2,21 @@ package com.poscodx.mysite.config.app;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+
+import com.poscodx.mysite.security.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
@@ -34,6 +42,8 @@ public class SecurityConfig {
    		.loginProcessingUrl("/user/auth")
    		.usernameParameter("email")
    		.passwordParameter("password")
+   		.defaultSuccessUrl("/")
+   		.failureUrl("/user/login")
    		.and()
 
    		.authorizeHttpRequests(registry -> {
@@ -48,4 +58,28 @@ public class SecurityConfig {
 
     	return http.build();
     }
+    
+    // Authentication Manager
+    @Bean
+    public AuthenticationManager authenticationManager(
+    		UserDetailsService userDetailsService, 
+    		PasswordEncoder passwordEncoder) {
+    	
+    	DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+    	authenticationProvider.setPasswordEncoder(passwordEncoder);
+    	authenticationProvider.setUserDetailsService(userDetailsService);
+    	
+    	return new ProviderManager(authenticationProvider);
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+    	return new BCryptPasswordEncoder(16);
+    }
+    
+    @Bean
+    public UserDetailsService userDetailService() {
+    	return new UserDetailsServiceImpl();
+    }
+    
 }
