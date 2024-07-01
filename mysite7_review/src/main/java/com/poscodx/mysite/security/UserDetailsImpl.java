031bcd13e438aa81103a0cb3c9d2a64182bb800b
CHOI HyoJun <cjfgh2449@gmail.com>
2024-07-01 20:14:12 +0900

    feat. mysite7 thymeleaf 적용 및 jar 패키징 #finish

---
+++ mysite7_review/src/main/java/com/poscodx/mysite/security/UserDetailsImpl.java
@@ -0,0 +1,58 @@
+package com.poscodx.mysite.security;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.poscodx.mysite.vo.UserVo;
+
+public class UserDetailsImpl extends UserVo implements UserDetails{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_" + getRole()));
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return super.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+
+}
---
+++ mysite7_review/src/main/java/com/poscodx/mysite/vo/BoardVo.java
@@ -0,0 +1,79 @@
+package com.poscodx.mysite.vo;
+
+public class BoardVo {
+	private Long no;
+	private String title;
+	private String contents;
+	private Integer hit;
+	private String regDate;
+	private Integer gNo;
+	private Integer oNo;
+	private Integer depth;
+	private Long userNo;
+	private String userName;
+	public Long getNo() {
+		return no;
+	}
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public Integer getHit() {
+		return hit;
+	}
+	public void setHit(Integer hit) {
+		this.hit = hit;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	public Integer getgNo() {
+		return gNo;
+	}
+	public void setgNo(Integer gNo) {
+		this.gNo = gNo;
+	}
+	public Integer getoNo() {
+		return oNo;
+	}
+	public void setoNo(Integer oNo) {
+		this.oNo = oNo;
+	}
+	public Integer getDepth() {
+		return depth;
+	}
+	public void setDepth(Integer depth) {
+		this.depth = depth;
+	}
+	public Long getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(Long userNo) {
+		this.userNo = userNo;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	
+
+	
+	
+}
