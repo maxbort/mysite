54f3ff896870251b6c229a7f6001860f59666f26
CHOI HyoJun <cjfgh2449@gmail.com>
2024-07-01 10:32:10 +0900

    feat. mysite6 #finish

--- mysite6_review/src/main/java/com/poscodx/mysite/config/SecurityConfig.java
+++ mysite6_review/src/main/java/com/poscodx/mysite/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
